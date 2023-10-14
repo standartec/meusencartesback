1dbb22fd4090e75b28ba1025a5f04acb78c96284
Fabricio Rezende Regis <rezenderegis@gmail.com>
2023-10-14 15:30:32 -0300

    Ajust image quality

--- src/modules/constants.ts
+++ src/modules/constants.ts
@@ -1,8 +1,8 @@
 //Dev.
-export const DIR_SAVE_IMAGES = '/Applications/MAMP/htdocs/images/posts';
-export const IMAGE_ADDRESS = 'http://localhost:8888/images/posts\/';
+//export const DIR_SAVE_IMAGES = '/Applications/MAMP/htdocs/images/posts';
+//export const IMAGE_ADDRESS = 'http://localhost:8888/images/posts\/';
 
 
 
-//export const DIR_SAVE_IMAGES = '/var/www/html/images/posts';
-//export const IMAGE_ADDRESS = 'http://postater.com/images/posts\/';
+export const DIR_SAVE_IMAGES = '/var/www/html/images/posts';
+export const IMAGE_ADDRESS = 'http://postater.com/images/posts\/';
