88d496b08b75b79a9f2d46b9ca0a11664d1a663b
Fabricio Rezende Regis <rezenderegis@gmail.com>
2024-01-10 01:51:22 -0300

    Adjust template to adapt new process of logo

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -118,8 +118,9 @@ export class ProductRepository extends Repository<Product> {
         const sql = `
         select p.description,
         
-        p.id_user as id_user_publish, p.header2, p.id_template as id_template1,p.*
-        from publish p 
+        p.id_user as id_user_publish, p.header2, p.id_template as id_template1,p.*, ul.image_link,ul.image_address, concat(ul.image_address,ul.image_link) as logo_publish
+        from publish p left join user_logo ul on ul.id = p.logo_id 
+
         where p.id_user = ` + idUser + ` and p.id = ` + idFlyer 
 
         console.log(sql)
